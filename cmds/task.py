f43d8c42459bace7a132d8e8e9c7cb874078ddce
ShireRen <53305571+ShireRen@users.noreply.github.com>
2019-08-29 17:23:32 +0800

    Add task

---
+++ cmds/task.py
@@ -0,0 +1,18 @@
+import discord
+import json,asyncio,datetime
+from discord.ext import commands
+from core.classes import Cog_Extension
+
+class Task(Cog_Extension)
+   def __init__(self,*args,**kwargs):
+       super().__init__(*args,**kwargs)
+
+       async def interval():
+           await self.bot.wait_until_ready()
+           self.channel = self.bot.get_channel()
+           while not self.bot.is_closed():
+               await self
+
+
+def setup(bot):
+    bot.add_cog(Task(bot))
