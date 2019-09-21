68bac6c95acc1044217781279ab8d428ff7195f9
ShireRen <53305571+ShireRen@users.noreply.github.com>
2019-09-21 15:15:28 +0800

    Add task

---
+++ cmds/task.py
@@ -0,0 +1,12 @@
+import discord
+import json,asyncio,datetime
+from discord.ext import commands
+from core.classes import Cog_Extension
+
+class Task(Cog_Extension):
+   def __init__(self,*args,**kwargs):
+       super().__init__(*args,**kwargs)
+
+
+def setup(bot):
+    bot.add_cog(Task(bot))
